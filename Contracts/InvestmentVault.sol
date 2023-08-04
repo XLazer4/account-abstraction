2d773fede4bb10bafeeb8c48c5842b35b4749a32
XLazer4 <khannarahul446@gmail.com>
2023-08-04 23:00:36 +0530

    deposit/withdraw funds

--- Contracts/InvestmentVault.sol
+++ Contracts/InvestmentVault.sol
@@ -6,14 +6,41 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract InvestmentVault is Ownable {
     struct Investor {
+        uint256 balance;
         uint256 amountAllowed;
         mapping(address => bool) vaultManagers;
     }
 
     mapping(address => Investor) public investors;
 
+    IERC20 public investmentToken;
+
     error UnauthorizedVaultManager();
     error ExceedsInvestorLimit();
+    error InsufficientInvestorBalance();
+    error TokenTransferFailed();
+
+    constructor(IERC20 _investmentToken) {
+        investmentToken = _investmentToken;
+    }
+
+    function deposit(uint256 _amount) public {
+        if (!investmentToken.transferFrom(msg.sender, address(this), _amount)) {
+            revert TokenTransferFailed();
+        }
+
+        investors[msg.sender].balance += _amount;
+    }
+
+    function withdraw(uint256 _amount) public {
+        if (investors[msg.sender].balance < _amount) {
+            revert InsufficientInvestorBalance();
+        }
+        investors[msg.sender].balance -= _amount;
+        if (!investmentToken.transfer(msg.sender, _amount)) {
+            revert TokenTransferFailed();
+        }
+    }
 
     function setAllowance(uint256 _amountAllowed) public {
         investors[msg.sender].amountAllowed = _amountAllowed;
